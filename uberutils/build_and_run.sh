e1a82de6109be981cdb904e4b0399ef25a4314ca
Antoine Pourchet <pourchet@uber.com>
2019-02-26 11:20:57 -0800

    Fixed build_and_run.sh

--- uberutils/build_and_run.sh
+++ uberutils/build_and_run.sh
@@ -1,1 +1,6 @@
-make kube-apiserver && cp _output/bin/kube-apiserver $GOPATH/bin/kube-apiserver && bash run-apiserver.sh 2>&1 | less
+#! /bin/bash
+set -ex
+
+make kube-apiserver
+cp _output/bin/kube-apiserver "$GOPATH/bin/kube-apiserver"
+bash uberutils/run-apiserver.sh 2>&1 | less
